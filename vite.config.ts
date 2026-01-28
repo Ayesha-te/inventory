7994af0ac0477f90ac7fa8cd6bb7cf2f1a28e59d
Ayesha-te <ayeshajahangirrr@gmail.com>
2026-01-28 18:54:30 +0500

    dsf

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    exclude: ['@monaco-editor/react', 'monaco-editor']
-  },
   build: {
     commonjsOptions: {
       include: [/node_modules/]
